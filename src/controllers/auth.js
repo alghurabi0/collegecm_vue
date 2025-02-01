3cfe2cf9f3b5c259e806f69925cbe38b51a1af32
alghurabi0 <zaid.alghurabi01@gmail.com>
2025-02-01 21:25:11 +0200

    auth4

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -18,14 +18,15 @@ export async function login(creds) {
     const response = await fetch('https://collegecm.work.gd/v1/login', {
       method: "POST",
       body: JSON.stringify(creds),
-      headers: "Content-Type:application/json",
+      headers: { 'Content-Type': 'application/json' },
     });
     if (!response.ok) {
-      return false;
+      const errorData = response.json();
+      return { user: null, err: errorData.err || 'حدث خطأ' };
     }
     const data = await response.json();
     if (data) {
-      return data.user;
+      return { user: data.user, err: null };
     }
   } catch (error) {
     console.error('Failed to fetch user data:', error);
